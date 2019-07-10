392f8bda699aee0292c48b6948d0591fb793778e
Nazmul <nazmul.abedin02@gmail.com>
2019-07-10 11:15:56 +0800

    Show profile name

--- resources/views/versions/index.blade.php
+++ resources/views/versions/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="page-header">
             <h1 class="page-title">
-                Versions
+               {{$profile->name}} - Versions
             </h1>
             <div class="ml-auto">
                 <a href="{{action('ProfileController@index')}}" class="ml-auto">
