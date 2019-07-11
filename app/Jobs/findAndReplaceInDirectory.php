4fabc342e942339c83ec2743383e152a21c0d50f
Nazmul <nazmul.abedin02@gmail.com>
2019-07-11 14:36:16 +0800

    Import porcess class

--- app/Jobs/findAndReplaceInDirectory.php
+++ app/Jobs/findAndReplaceInDirectory.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Symfony\Component\Process\Process;
 
 class findAndReplaceInDirectory implements ShouldQueue
 {
